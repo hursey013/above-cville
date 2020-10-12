13b9cb85aab284789b1c0617225543dd6f4e360d
Brian Hurst <hursey013@protonmail.com>
2020-10-12 17:46:34 -0400

    Add twit

--- clock.js
+++ clock.js
@@ -25,6 +25,15 @@ admin.initializeApp({
 const db = admin.database();
 const ref = db.ref("states");
 
+// Init Twit
+const Twit = require("twit");
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 // Init Axios
 const axios = require("axios").default;
 const instance = axios.create({
